a891fa367f421aa203a8dfe57cab7f49b375ecc9
ykolodiy <yuriy.kolodiy@gmail.com>
2016-08-22 09:24:53 -0500

    Add files via upload

--- writeExcel2.java
+++ writeExcel2.java
@@ -2,16 +2,21 @@ package writeExcel;
 //tut zapysujem w eksel
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.IOException;
 
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class writeExcel2 {
 
-	public static void main(String[] args) throws Exception {
+	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
-
+		System.setProperty("webdriver.chrome.driver", "C:\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
 		// apache POI
 		//locate file
 		File src = new File ("C:\\Users\\vkoloyu\\test.xlsx");
@@ -22,14 +27,18 @@ public class writeExcel2 {
 		//focus on sheet1 index0
 		XSSFSheet sheet1 = wb.getSheetAt(0);
 	//row1 col1
-		
+
 		//WRITING!!!!!!!!!!!!!!!!!!!!
+		driver.get("http://www.yahoo.com");
+		String title = driver.getTitle();
+		sheet1.getRow(0).createCell(2).setCellValue(title);
 		
-		sheet1.getRow(0).createCell(2).setCellValue("Pass");
-		sheet1.getRow(1).createCell(2).setCellValue("Fail");
 		//saving file
 		FileOutputStream fout = new FileOutputStream (src);
 		wb.write(fout);
+		
+		driver.quit();
+		
 		
 		wb.close();
 	}
